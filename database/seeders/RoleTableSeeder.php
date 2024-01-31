41266e9387c968fad07654547b85204374d2b6b5
Umar Muqimuddin <37995491+umar181g1@users.noreply.github.com>
2024-01-31 23:16:18 +0700

    featue: setup Spatie

--- database/seeders/RoleTableSeeder.php
+++ database/seeders/RoleTableSeeder.php
@@ -4,17 +4,18 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
-class DatabaseSeeder extends Seeder
+class RoleTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(RoleTableSeeder::class);
-        $this->call(UserTableSeeder::class);
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'user']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,16 @@ use Inertia\Inertia;
 |
 */
 
+Route::get('/admin', function() {
+    return 'Hai admin';
+})->middleware('role:admin');
+
+Route::get('/user', function() {
+    return 'Hai user';
+})->middleware('role:user');
+
+
+
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
